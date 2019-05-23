72307d7c2dcb777cafd02c6aff0debb76a4f644f
GustavoVenassi <guvenassi@gmail.com>
2019-05-23 16:23:58 -0300

    first commit

---
+++ src/cifradecesar/cifradecesar.java
@@ -0,0 +1,11 @@
+
+package cifradecesar;
+
+public class cifradecesar {
+    
+    public static void main(String[]args){
+    
+    cifra cifrar  = new cifrar();
+    cifrar.setVisible(true);
+    } 
+}
